8bd53da81c00c28a56f7156faa833294d795cf2f
The-Abhinav-Shahi <152834424+The-Abhinav-Shahi@users.noreply.github.com>
2024-03-10 00:45:18 +0545

    before dropdown menu

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 function menubar() {
-    alert('You clicked menubar.');
+
+
+    document.getElementById("mySidenav").style.width = "20%";
+    document.getElementById("main").style.marginLeft = "20%";
 }
 
 function notifications() {
@@ -8,4 +11,40 @@ function notifications() {
 
 function myAccount() {
     alert('Your account is great.')
+}
+
+function homePage() {
+    window.location = "index.html";
+}
+
+function overViewPage() {
+    window.location = "employeeOverViewPage.html";
+}
+
+// var dropdown = document.getElementsByClassName("dropdown-btn");
+// var i;
+
+// for (i = 0; i < dropdown.length; i++) {
+//   dropdown[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var dropdownContent = this.nextElementSibling;
+//     if (dropdownContent.style.display === "block") {
+//       dropdownContent.style.display = "none";
+//     } else {
+//       dropdownContent.style.display = "block";
+//     }
+//   });
+var dropdown = document.getElementsById("employee-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if(dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    })
 }
