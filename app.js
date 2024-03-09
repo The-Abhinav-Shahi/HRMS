24d7d37bd49387004f126d175826cca7b7393d6b
The-Abhinav-Shahi <152834424+The-Abhinav-Shahi@users.noreply.github.com>
2024-03-09 17:20:26 +0545

    navbar finished

---
+++ app.js
@@ -0,0 +1,11 @@
+function menubar() {
+    alert('You clicked menubar.');
+}
+
+function notifications() {
+    alert('You have notifications.');
+}
+
+function myAccount() {
+    alert('Your account is great.')
+}
